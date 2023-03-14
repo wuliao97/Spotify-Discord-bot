70d1b0422198df2d2ff6936aa47c672f3845e547
wuliao97 <117802193+wuliao97@users.noreply.github.com>
2023-03-14 18:39:12 +0900

    add and remove some functions and classes

--- funcs.py
+++ funcs.py
@@ -3,15 +3,10 @@ import config as cfg
 
 import discord
 from discord.ui import *
-from discord.commands import Option
-from discord.ext import commands, tasks
-import discord_timestamps as dts
+from discord.ext import commands
 from discord_timestamps.formats import TimestampType
-import os, re, io, sys,json
-import spotipy, logging, requests, datetime, asyncio
+
 import datetime
-from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
 
 
 
@@ -32,37 +27,38 @@ class TimeFormat:
 
 
 
-"""For Spotify, send a Song URL"""
-class SpotifyView(discord.ui.View):
-    def __init__(self, spotify, ephemeral:bool):
-        super().__init__()
-        self.sp = spotify
-        self.ephemeral = ephemeral
-
-    @discord.ui.button(label="URL", style=discord.ButtonStyle.green, emoji=cfg.SP_EMOJI)
-    async def callback(self, button:discord.Button, interaction:discord.Interaction):
-        button.disabled=True
-        await interaction.response.edit_message(view=self)
-        await interaction.followup.send(self.sp.track_url, ephemeral=self.ephemeral)
-
-
-
 class SpotifySelect(discord.ui.Select):
     def __init__(self, placeholder: str, options: list[discord.SelectOption], disabled: bool = False):
         super().__init__(placeholder=placeholder, options=options, disabled=disabled)
 
     async def callback(self, inter:discord.Interaction):
-        song = self.values
-        self.disabled = True
-        msg = await inter.response.send_message(song)
-
-
+        song = self.values[0]
+        await inter.response.send_message(song)
 
 
 class SpotifySelectView(discord.ui.View):
-    def __init__(self, placeholder, option, timeout: float = 180, disable_on_timeout: bool = True):
+    def __init__(self, placeholder, option, url,timeout: float = 180, disable_on_timeout: bool = True):
         super().__init__(timeout=timeout, disable_on_timeout=disable_on_timeout)
         self.add_item(SpotifySelect(placeholder=placeholder, options=option))
+        self.add_item(Button(label="URL", url=url, emoji=cfg.SP_EMOJI))
+
+
+
+class SpotifyButtonS(discord.ui.View):
+    def __init__(self, mode = "default", url = None | list):
+        super().__init__()
+
+        labels = []
+
+        if mode == "default" and len(url) == 1:
+            labels.extend(["Jump"])
+            
+        elif mode == "urls":
+            labels.extend(["Song", "Album", "Artist"])
+            
+        for i, label in zip(url, labels):
+            self.add_item(Button(label=label, url=i, emoji=cfg.SP_EMOJI))
+
 
 
 
@@ -77,11 +73,10 @@ def spotify(interact:discord.Interaction, user:discord.Member):
     user = user if user else interact.user
 
     if material:= next((activity for activity in user.activities if isinstance(activity, discord.Spotify)), None):
-
         artists = material.artists
         artists = ", ".join(artists) if isinstance(artists, list) else material.artist
         artists = str(artists).translate(str.maketrans({"[":"", "]":"", "'":""}))
-        
+
         return True, material, artists
     else:
         return (
@@ -91,11 +86,32 @@ def spotify(interact:discord.Interaction, user:discord.Member):
         )
 
 
+def data_sort(title:str, album:str) -> str:
+    title = title.translate(str.maketrans({"[":"(", ")":")"}))
+    title = title[:17] + "..." if len(title) > 17 else title
+    album = album[:15] + "..." if len(album) > 15 else album
+
+    return title, album
+
+
+
+def spotify_extract(id)->list[str]:
+    result = cfg.sp.track(id)
+    
+    song_url = result['external_urls']['spotify']
+    album_url = result['album']['external_urls']['spotify']
+    artist_url = result['artists'][0]['external_urls']['spotify']
+
+    return [song_url, album_url, artist_url]
+
+
 
 def sand_symbol(literal, symble="*", count = 1):
     return (symble * count) + str(literal) + (symble * count)
 
 SS = sand_symbol
+
+
 
 def time_func(PLACE:str = "JST"):
     data = datetime.datetime.now()
